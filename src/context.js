6fe34e0e4e07c9497b8a4464abf1488d9e4f14b3
Hope <apple@jerry.com>
2020-03-11 06:34:50 +0800

    detail,landing page added

---
+++ src/context.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react'
+import { storeProducts, detailProduct } from './data';
+import Product from './components/Product';
+
+const ProductContext = React.createContext();
+//Provider
+//Consumer
+
+class ProductProvider extends Component {
+    state = {
+        products : [],
+        detailProduct:detailProduct,
+        cart:[],
+        modalOpen: false,
+        modalProduct:detailProduct,
+    };
+
+    componentDidMount() {
+        this.setProducts();
+    }
+    
+    setProducts = () => {
+        let tempProducts = [];
+        storeProducts.forEach(item => {
+            const singleItem = { ...item };
+            tempProducts = [ ...tempProducts, singleItem ];
+        })
+        this.setState(() => {
+            return { products : tempProducts }
+        })
+    }
+
+    getItem = id => {
+        const product = this.state.products.find(item => item.id === id);
+        return product;
+    };
+
+    handleDetail = id => {
+        const product = this.getItem(id);
+        this.setState(()=>{
+            return {detailProduct:product}
+        })
+    };
+
+    addToCart = (id) => {
+        let tempProducts = [...this.state.products];
+        const index = tempProducts.indexOf(this.getItem(id));
+        const product = tempProducts[index];
+        product.inCart = true;
+        product.count = 1;
+        const price = product.price;
+        product.total = price;
+        this.setState(
+            () => {
+                return { products:tempProducts, cart:[...this.state.cart, product] };
+            },
+            () => {
+                console.log(this.state);
+            }
+            );
+    };
+
+    openModal = id =>{
+        const product = this.getItem(id);
+        this.setState(()=>{
+            return {modalProduct: product, modalOpen: true}
+        })
+    }
+    
+    closeModal = () =>{
+        this.setState(()=>{
+            return {modalOpen:false}
+        })
+    }
+    
+    tester = () => {
+        console.log('State products : ', this.state.products[0].inCart);
+        console.log('Data products : ', storeProducts[0].inCart);
+
+        const tempProducts = [...this.state.products];
+        tempProducts[0].inCart = true
+        this.setState(() => {
+            return { proudcts: tempProducts }
+        }, () => {
+            console.log('State products : ', this.state.products[0].inCart);
+            console.log('Data products : ', storeProducts[0].inCart);
+        })
+     }
+
+    render() {
+        return (
+            <ProductContext.Provider 
+                value = {{ 
+                    handleDetail:this.handleDetail, 
+                    addToCart: this.addToCart,
+                    ...this.state,
+                    openModal: this.openModal,
+                    closeModal: this.closeModal
+                }}
+            >
+              { this.props.children }  
+              <button onClick = { this.tester }>test me</button>
+              {/* { this.props.children } */}
+            </ProductContext.Provider>
+        )
+    }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export { ProductProvider, ProductConsumer };
